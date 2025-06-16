2a0c91ae847671bc4083ae6a1d3bd92c854ca322
Yakir Uzan <yakiruzangreen@gmail.com>
2025-06-16 09:58:56 +0300

    added sensors file - termal, cellular, motion

---
+++ Models/MotionSensor.cs
@@ -0,0 +1,8 @@
+﻿namespace SensorsProject
+{
+    class CellularSensor : Sensor
+    {
+        //קלאס ליצירת סנסור תנועה שיורש מקלאס סנסור
+        public CellularSensor() : base("Motion") { }
+    }
+}
---
+++ Models/TermalSensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorsProject
+{
+    //קלאס ליצירת סנסור טרמי שיורש מקלאס סנסור
+    class ThermalSensor : Sensor
+    {
+        public ThermalSensor() : base("termal") { }
+    }
+}
+
