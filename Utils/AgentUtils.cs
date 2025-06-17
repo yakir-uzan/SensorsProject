b72ad13780489162ec91a8e902908c57f7de74be
Yakir Uzan <yakiruzangreen@gmail.com>
2025-06-17 23:24:45 +0300

    added agentUtils file

---
+++ Utils/AgentUtils.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SensorsProject
+{
+    public static class AgentUtils
+    {
+        //רשימה כללית של כל הסנסורים
+        public static List<string> AllSensors = new List<string>
+        { "Thermal", "Audio", "Pulse", "Motion", "Magnetic", "Cellular", "Light", "Single"};
+    }
+}
